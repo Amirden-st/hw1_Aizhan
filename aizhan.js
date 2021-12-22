659386e9ec0cf20cc38175e2c82220bd31e4160a
jxxllay <isakova.aizhan1208@gmail.com>
2021-12-23 00:52:41 +0600

    fvnr

---
+++ aizhan.js
@@ -0,0 +1,20 @@
+let name=prompt(" Введите Имя и Фамилию")
+alert('Приветствую ' + "Aizhan")
+let number1 = 3
+let number2=5
+if(number1<number2){
+    console.log("5 больше")
+}
+else{
+    console.log("3 меньше")
+}
+let age=19
+if(age<16){
+    console.log("Ты школьник")
+}
+else if (age<=18){
+    console.log("ты либо школьник, либо студент")
+} else {
+    console.log("Ты либо студент, либо работаешь")
+}
+
